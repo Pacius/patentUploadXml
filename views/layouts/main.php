d0b3f5b6fff9db255b3023ae951904a16ad9e590
Андрей Гавриленко <icecrown.gm@gmail.com>
2019-01-22 00:32:33 +0300

    скролл

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -93,6 +93,22 @@ $curRoute = Yii::$app->controller->route;
         text-align: center;
         width: 100%;
     }
+
+    ::-webkit-scrollbar {
+        width: 12px;
+    }
+
+    ::-webkit-scrollbar-track {
+        //-webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3);
+        //border-radius: 10px;
+    }
+
+    ::-webkit-scrollbar-thumb {
+        border-radius: 6px;
+        background-color: #dddddd;
+        width: 4px;
+    }
+
 </style>
 
 <div class="container">
